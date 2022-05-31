21d7407c9ac5a4ba07a6fbde8b0913e1c6c20563
Rohit Abhishek <rabhi1611@gmail.com>
2022-05-31 13:39:49 +0530

    made secret key unexposable

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fw#g!k^og(ud9xx)3yro0%05sd@k@=d*=dpi2^3v$j$d)j*kk2'
+
+from dotenv import load_dotenv
+load_dotenv()
+
+#SECRET_KEY = 'django-insecure-fw#g!k^og(ud9xx)3yro0%05sd@k@=d*=dpi2^3v$j$d)j*kk2'
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
